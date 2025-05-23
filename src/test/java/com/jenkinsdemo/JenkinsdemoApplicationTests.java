8210f3333520be34c828898cba1571012983b9b4
hanamant chadchan <hanmantchadchan@gmail.com>
2025-05-23 13:47:58 +0530

    Adding first commit

---
+++ src/test/java/com/jenkinsdemo/JenkinsdemoApplicationTests.java
@@ -0,0 +1,27 @@
+package com.jenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsdemoApplicationTests {
+
+	public static Logger logger= LoggerFactory.getLogger(JenkinsdemoApplicationTests.class);
+
+	@Test
+	public void contextLoads() {
+		
+		assertEquals(true,true);
+	}
+
+	private void assertEquals(boolean b, boolean c) {
+		// TODO Auto-generated method stub
+		logger.info("Test case executing....");
+		
+	}
+
+
+
+}
